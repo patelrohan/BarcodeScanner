eb9a2ea35c076d0b8e534c06c2d06b1cffec0dca
Rohan Patel <patelrohan@users.noreply.github.com>
2025-01-20 20:56:32 -0500

    Initial Commit

---
+++ BarcodeScanner/BarcodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeScannerApp.swift
+//  BarcodeScanner
+//
+//  Created by Rohan Patel on 1/20/25.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
