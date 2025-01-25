9fd8e6dd64fa53a6317a87db03716fdba59ee80f
Rohan Patel <patelrohan@users.noreply.github.com>
2025-01-24 23:19:11 -0500

    Logic for Alerts, barcode text changes

--- BarcodeScanner/BarcodeScannerView.swift
+++ BarcodeScanner/BarcodeScannerView.swift
@@ -7,11 +7,32 @@
 
 import SwiftUI
 
+struct AlertItem: Identifiable{
+    let id = UUID()
+    let title: String
+    let message: String
+    let dismissButton: Alert.Button
+}
+
+struct AlertContext{
+    static let invalidDeviceInput = AlertItem(title: "Device not supported",
+                                              message: "Could not find a valid video capture device.",
+                                              dismissButton: .default(Text("OK")))
+    
+    static let invalidScannedItem = AlertItem(title: "Invalid scanned item",
+                                              message: "Scanned value is invalid. Scanner only supports UPC and barcode.",
+                                              dismissButton: .default(Text("OK")))
+}
+
 struct BarcodeScannerView: View {
+    
+    @State private var scannedCode = ""
+    @State private var alertItem: AlertItem?
+    
     var body: some View {
         NavigationView {
             VStack {
-                Rectangle()
+                ScannerView(scannedBarcode: $scannedCode, alertItem: $alertItem)
                     .frame(maxWidth: .infinity, maxHeight: 300)
                 
                 Spacer().frame(height: 50)
@@ -19,13 +40,18 @@ struct BarcodeScannerView: View {
                 Label("Scan Barcode", systemImage: "barcode.viewfinder")
                     .font(.title3)
                 
-                Text("Barcode Not Scanned")
-                    .foregroundStyle(.red)
+                Text(scannedCode.isEmpty ? "Barcode Not Scanned" : scannedCode)
+                    .foregroundStyle(scannedCode.isEmpty ? .red : .green)
                     .fontWeight(.bold)
                     .font(.title)
                     .padding()
             }
             .navigationTitle("Barcode Scanner")
+            .alert(item: $alertItem) { alertItem in
+                Alert(title: Text(alertItem.title),
+                      message: Text(alertItem.message),
+                      dismissButton: alertItem.dismissButton)
+            }
         }
     }
 }
